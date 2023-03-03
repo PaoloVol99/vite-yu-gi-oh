4241762a8511f64a82d5333582a15b7a274843e9
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-03-03 17:11:44 +0100

    FEAT API data printed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,22 @@
 <script>
 import axios from 'axios'
 
+import Card from './Card.vue'
+
 export default {
+    components: {
+        Card
+    },
+    data() {
+        return {
+            cards: [],
+        }
+    },
     created() {
         axios
             .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0')
             .then((res) => {
-                console.log(res)
+                this.cards = res.data.data
             })
     }
 }
@@ -15,9 +25,22 @@ export default {
 
 <template>
 
+    <main>
+        <div class="container">
+            <div class="row">
+                <div v-for="(card, i) in cards" :key="i" class="col">
+                    <Card :imageSrc="card.card_images[0].image_url" :name="card.name" :archetype="card.archetype" />
+                </div>
+            </div>
+        </div>
+    </main>
 
 </template>
 
 <style lang="scss" scoped>
+
+.col {
+    width: calc(100% / 5);
+}
 
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    props: {
+        imageSrc: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        archetype: {
+            type: String,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<template>
+
+    <div class="card">
+        <img class="card-img" :src="imageSrc" alt="">
+        <span class="card-name">{{ name }}</span>
+        <span class="card-archetype">{{ archetype }}</span>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.card {
+    text-align: center;
+    background-color: orange;
+    height: 100%;
+    .card-img {
+        display: block;
+    }
+    .card-name {
+        display: block;
+    }
+    .card-archetype {
+        display: block;
+    }
+}
+
+</style>
