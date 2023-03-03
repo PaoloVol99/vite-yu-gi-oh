d4553a456d7fe162c99e7ef3ce18ae448fd855c7
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-03-03 16:45:02 +0100

    FEAT components and API call added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
+  components: {
+    AppHeader,
+    AppMain,
+  }
 
 }
 </script>
 
 <template>
-  
+  <AppHeader />
+  <AppMain />
+
 </template>
 
 <style lang="scss">
+@use './style/general.scss';
+
+
 
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios'
+
+export default {
+    created() {
+        axios
+            .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0')
+            .then((res) => {
+                console.log(res)
+            })
+    }
+}
+</script>
+
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
